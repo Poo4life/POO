2ff25ed0160ade1951e1a411df2656421d2f5270
FIXOLAZ <fixolaz@hotmail.com>
2019-12-02 19:32:41 +0000

    Main
    Fiz este main ontem, vejam lá se está porreiro, é só fazer as funções para cada classe e done

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -1,9 +1,59 @@
-
 package main;
+import java.util.Scanner; //Scanner Class
 
 public class Main {
-
-    public static void main(String[] args) {
-    }
     
+    public static void main(String[] args) {
+        int opcao;
+        Scanner teclado = new Scanner(System.in); //Input
+        
+        do{
+            //Menu
+            System.out.println("-----------------");
+            System.out.println("Menu Principal");
+            System.out.println("-----------------");
+            System.out.println("1 - Inserir Dados");
+            System.out.println("2 - Editar Dados");
+            System.out.println("3 - Apagar Dados");
+            System.out.println("4 - Consultar");
+            System.out.println("5 - Listar");
+            System.out.println("");
+            System.out.println("0 - Sair");
+            System.out.println("-----------------");
+            System.out.println("");
+            System.out.println("Introduza a opção pretendida.");
+            
+            opcao= teclado.nextInt();
+            
+            switch (opcao) {
+                    
+                case 1:
+                    System.out.println("Inserir Dados"); //Estes souts são só exemplos, foi para testar, aqui depois se calhar entra uma função só para inserir.
+                    break;
+
+                case 2:
+                    System.out.println("Editar");
+                    break;
+
+                case 3:
+                    System.out.println("Apagar");
+                    break;
+
+                case 4:
+                    System.out.println("Consultar");
+                    break;
+
+                case 5:
+                    System.out.println("Listar"); 
+                    break;
+                
+                case 0:
+                    System.out.println("A fechar...");
+                    System.err.println("Programa encerrado.");
+                default:
+                    System.err.println("Opção inválida");
+            }                
+        }while (opcao != 0);
+    }
 }
+    
